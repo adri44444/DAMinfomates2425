92a95d88b017153af78d78b3e55afe854da2faae
Adri <adriacruz_@ceroca.cat>
2024-09-26 19:26:24 +0200

    jugador disparant

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -8,6 +8,8 @@ public class NauJugador : MonoBehaviour
 
     private Vector2 minPantalla, maxPantalla;
 
+    [SerializeField] private GameObject prefabProjectil;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,35 +17,51 @@ public class NauJugador : MonoBehaviour
 
         minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
         maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
-        float midaMeitatImatgeX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x  / 2;
-        float midaMeitatImatgeY = GetComponent<SpriteRenderer>().bounds.size.y /2;
+        float midaMeitatImatgeX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x / 2;
+        float midaMeitatImatgeY = GetComponent<SpriteRenderer>().bounds.size.y / 2;
 
 
         // minPantalla.x = minPantalla.x + 0.75f;
         // minPantalla.x += 0.75f; // Es sinonim a la linia de dalt.
-        minPantalla.x  += midaMeitatImatgeX;
-        maxPantalla.x  -= midaMeitatImatgeX;
-        minPantalla.y += midaMeitatImatgeY; 
+        minPantalla.x += midaMeitatImatgeX;
+        maxPantalla.x -= midaMeitatImatgeX;
+        minPantalla.y += midaMeitatImatgeY;
         maxPantalla.y -= midaMeitatImatgeY;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovimentNau();
+        DisparaProjectil();
+
+
+    }
+
+    private void DisparaProjectil()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject projectil = Instantiate(prefabProjectil);
+            projectil.transform.position = transform.position;
+        
+        }
+    }
+    private void MovimentNau()
+    {
         float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccioIndicadaY = Input.GetAxisRaw("Vertical");
         //Debug.Log("X:" + direccioIndicadaX + "-Y:"+ direccioIndicadaY);
 
-        Vector2 direccioIndicada = new Vector2(direccioIndicadaX,direccioIndicadaY).normalized;
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
 
         Vector2 novaPos = transform.position;
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
 
-        novaPos.x = Mathf.Clamp(novaPos.x,minPantalla.x,maxPantalla.x);
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
         novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
 
         transform.position = novaPos;
-        
     }
 }
