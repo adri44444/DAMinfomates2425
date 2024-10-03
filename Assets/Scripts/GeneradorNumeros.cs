6deb565186d0a368f84101ee2a9c6012d548bb0e
Adri <adriacruz_@ceroca.cat>
2024-10-03 17:04:11 +0200

    Generar enemics

---
+++ Assets/Scripts/GeneradorNumeros.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorNumeros : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabNumero;
+
+    private Vector2 minPantalla, maxPantalla;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+        InvokeRepeating("GenerarNumero",1f,2f);
+
+
+    }
+
+    private void GenerarNumero()
+    {
+        GameObject numero = Instantiate(prefabNumero);
+
+        numero.transform.position = new Vector2(Random.Range(minPantalla.x,maxPantalla.x),maxPantalla.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
